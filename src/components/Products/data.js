18a5cd29c5a91c96342df584caf56df003ef7d2f
Pablo Derluk <64289829+PabloDerluk88@users.noreply.github.com>
2020-12-17 14:08:56 -0300

    perrayo

---
+++ src/components/Products/data.js
@@ -0,0 +1,63 @@
+import product1 from '../../images/product-1.jpg'
+import product2 from '../../images/product-2.jpg'
+import product3 from '../../images/product-3.jpg'
+
+import desert1 from '../../images/desert-1.jpg'
+import desert2 from '../../images/desert-2.jpg'
+import desert3 from '../../images/desert-3.jpg'
+
+export const productData = [
+    {
+        img: product1,
+        alt: 'Sushi',
+        name: 'Rainbow',
+        desc: '5 Hikaru, 5 Kawa, 5 Abocado ,5 New York, 5 Philadelfia, 5 Niguiri Salmón',
+        price: '$90.00',
+        button: 'Add to Cart'
+    },
+    {
+        img: product2,
+        alt: 'Sushi',
+        name: 'Miyuki',
+        desc: '10 Sashimi Salmón, 5 Niguiri Salmón, 5 Niguiri Langostino, 10 Philadelfia, 10 Kawa, 5 Sakemaki',
+        price: '$89.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: product3,
+        alt: 'Sushi',
+        name: 'Akari',
+        desc: '10 Hikaru, 10 Philadelfia, 10 Salmón Hot, 10 Sashimi de Salmón, 10 Niguiri de Salmón',
+        price: '$75.99',
+        button: 'Add to Cart'
+    }
+
+]
+
+export const productDataTwo = [
+    {
+        img: desert1,
+        alt: 'Ice Cream',
+        name: 'Seven Heaven',
+        desc: 'Chocolate and Vanilla, Coffee and Oreo, Peanut Butter and Jelly',
+        price: '$90.00',
+        button: 'Add to Cart'
+    },
+    {
+        img: desert2,
+        alt: 'Flan',
+        name: 'Argentine Flan',
+        desc: 'Flan with Dulce de Leche from Buenos Aires, Argentina! give it a try :D',
+        price: '$89.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: desert3,
+        alt: 'Chocotorta',
+        name: 'Chocotorta',
+        desc: ' A Traditional Argentine Dessert. Taste it under your own risk because it wont be the last time',
+        price: '$75.99',
+        button: 'Add to Cart'
+    }
+
+]
